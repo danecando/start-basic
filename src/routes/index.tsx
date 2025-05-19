ef186f53fef5b0324e72f86d7d144ff966eb4192
Dane Grant <danecando@gmail.com>
2025-05-19 07:53:01 -0400

    cleanup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,23 @@
+import { createServerFn } from "@tanstack/react-start"
+
+const helloWorld = createServerFn({ type: "static" }).handler(() => {
+  return {
+    message: "Hello World!!!",
+  }
+}
+)
+
 export const Route = createFileRoute({
   component: Home,
+  loader: async () => await helloWorld(),
 })
 
 function Home() {
+  const { message } = Route.useLoaderData()
   return (
     <div className="p-2">
       <h3>Welcome Home!!!</h3>
+      {message}
     </div>
   )
 }
